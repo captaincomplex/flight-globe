ccb0eedc5a492926e47311044efb28e2f95a0826
captaincomplex <63202949+captaincomplex@users.noreply.github.com>
2025-07-06 18:16:10 +0100

    Add files via upload

---
+++ three-globe.module.js
@@ -0,0 +1,12 @@
+export default function ThreeGlobe() {
+  return {
+    globeImageUrl: () => this,
+    arcsData: () => this,
+    arcColor: () => this,
+    arcDashLength: () => this,
+    arcDashGap: () => this,
+    arcDashInitialGap: () => this,
+    arcDashAnimateTime: () => this,
+    arcAltitudeAutoScale: () => this
+  };
+}
