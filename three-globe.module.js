fa267aa17f0007bedda136b1bac28bac520ae278
captaincomplex <63202949+captaincomplex@users.noreply.github.com>
2025-07-06 18:19:24 +0100

    Add files via upload

--- three-globe.module.js
+++ three-globe.module.js
@@ -1,12 +1,14 @@
+import * as THREE from 'https://unpkg.com/three@0.150.0/build/three.module.js';
+
 export default function ThreeGlobe() {
-  return {
-    globeImageUrl: () => this,
-    arcsData: () => this,
-    arcColor: () => this,
-    arcDashLength: () => this,
-    arcDashGap: () => this,
-    arcDashInitialGap: () => this,
-    arcDashAnimateTime: () => this,
-    arcAltitudeAutoScale: () => this
-  };
+  const globeObj = new THREE.Object3D();
+  globeObj.globeImageUrl = () => globeObj;
+  globeObj.arcsData = () => globeObj;
+  globeObj.arcColor = () => globeObj;
+  globeObj.arcDashLength = () => globeObj;
+  globeObj.arcDashGap = () => globeObj;
+  globeObj.arcDashInitialGap = () => globeObj;
+  globeObj.arcDashAnimateTime = () => globeObj;
+  globeObj.arcAltitudeAutoScale = () => globeObj;
+  return globeObj;
 }
